3f7cd74a65058ccacd1ff2d61024b1a9080a9ea2
Sacchidanand Verma <sachin11197@gmail.com>
2020-04-28 10:07:17 +0530

    s2 added something

--- hello.cpp
+++ hello.cpp
@@ -4,6 +4,13 @@ using namespace std;
 void fun(){
 	cout<<"SDE1 added fun()\n";
 }
+
+//s2 added :
+void fun2(){
+	cout<<"Yes s2 added fun2();\n";
+
+
+}
 int main(){
 	cout<<"Hello world\n";
 	fun();
